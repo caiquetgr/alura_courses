01766d3f2f941acbdc7c3d8c4c79c6c798fbce06
Caique Aquino Borges - CBO <caique.borges@iteris.local>
2020-05-18 23:28:23 -0300

    Part 2 and 3 of Dart OO course

--- dart/oo/alurabank/bin/ContaCorrente.dart
+++ dart/oo/alurabank/bin/ContaCorrente.dart
@@ -4,5 +4,27 @@ class ContaCorrente {
   String titular;
   int agencia;
   int conta;
-  Money saldo;
+  Money saldo = Money.fromDouble(0.0, Currency('BRL'));
+
+  bool transferencia(Money valorTransferencia, ContaCorrente contaDestino) {
+    if (saque(valorTransferencia)) {
+      contaDestino.deposito(valorTransferencia);
+    }
+  }
+
+  bool saque(Money valorDoSaque) {
+    if (saldo.amount - valorDoSaque.amount < -100) {
+      print('Saldo insuficiente');
+      return false;
+    } else {
+      saldo -= valorDoSaque;
+      print('Novo saldo de ${titular}: ${saldo}');
+      return true;
+    }
+  }
+
+  Money deposito(Money valorDeposito) {
+    saldo += valorDeposito;
+    return saldo;
+  }
 }
--- dart/oo/alurabank/bin/main.dart
+++ dart/oo/alurabank/bin/main.dart
@@ -3,17 +3,26 @@ import 'package:money/money.dart';
 import 'ContaCorrente.dart';
 
 void main() {
-
   ContaCorrente contaCorrente = ContaCorrente();
   contaCorrente.titular = "Caique";
   contaCorrente.agencia = 1234;
   contaCorrente.conta = 12345678;
-  contaCorrente.saldo = Money.fromDouble(120.20, Currency("BRL"));
+  contaCorrente.saldo = Money.fromDouble(120.20, Currency('BRL'));
 
   print("Titular: ${contaCorrente.titular}");
   print("Agencia: ${contaCorrente.agencia}");
   print("Conta: ${contaCorrente.conta}");
   print("Saldo: ${contaCorrente.saldo}");
 
-}
+  ContaCorrente contaCorrente2 = ContaCorrente();
+  contaCorrente2.titular = "Caique";
+  contaCorrente2.agencia = 1234;
+  contaCorrente2.conta = 12345678;
+  contaCorrente2.saldo = Money.fromDouble(120.20, Currency('BRL'));
 
+  print(contaCorrente.hashCode);
+  print(contaCorrente2.hashCode);
+
+  contaCorrente.saque(Money.fromString("100.00", Currency('BRL')));
+
+}
