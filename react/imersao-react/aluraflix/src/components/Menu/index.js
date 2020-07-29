9111a6c9899749780cbb451ce667ebe967dd0300
Caique Aquino Borges - CBO <caique.borges@iteris.local>
2020-07-28 21:34:48 -0300

    Segunda aula Imersão React

--- react/imersao-react/aluraflix/src/components/Menu/index.js
+++ react/imersao-react/aluraflix/src/components/Menu/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Logo from '../../assets/img/logo.png';
 import './Menu.css';
 import Button from '../Button';
+import { Link } from 'react-router-dom';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="AluraFlix logo"></img>
-            </a>
-            <Button as="a" href="/" className="ButtonLink">
+            </Link>
+            <Button as={Link} to="/" className="ButtonLink">
                 Novo vídeo
             </Button>
         </nav>
