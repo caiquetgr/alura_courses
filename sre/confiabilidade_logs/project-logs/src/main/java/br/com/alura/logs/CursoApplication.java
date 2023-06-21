c6f118df361ccf2825108b524168a3f2237cf0f6
Caique Borges <caiquetgr@gmail.com>
2023-06-21 09:27:02 -0300

    sre logs: java source code

---
+++ sre/confiabilidade_logs/project-logs/src/main/java/br/com/alura/logs/CursoApplication.java
@@ -0,0 +1,17 @@
+package br.com.alura.logs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(CursoApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("Iniciando a API de cursos Alura");
+		SpringApplication.run(CursoApplication.class, args);
+	}
+}
